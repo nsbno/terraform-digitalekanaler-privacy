efcdc43945e5d866139c91e0556506dd07ecb7de
erlendgjelsvik <erlendgje@gmail.com>
2024-12-06 16:06:30 +0100

    Add insight

--- output.tf
+++ output.tf
@@ -6,3 +6,7 @@ output "sqs_queue_url" {
 output "sns_privacy_audit_arn" {
   value = data.aws_sns_topic.privacy_audit.arn
 }
+
+output "s3_privacy_bucket" {
+  value = data.aws_s3_bucket.privacy_bucket.bucket
+}
