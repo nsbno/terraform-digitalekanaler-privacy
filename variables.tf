870252bb1aaa2cd8aa717016dd81d398700974ab
Torbjørn Sørdal <torbjorn.o.s@hotmail.com>
2026-02-04 11:35:46 +0100

    [patch] Add booking as privacy service

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "privacy_service_enum" {
   validation {
     condition = contains([
       "USER", "FORM", "LOYALTY", "SMARTPRIS", "TAXI", "EUROPA", "PUSH", "SUBSCRIPTION", "TICKET", "ID_TICKET",
-      "RECEIPT", "SEAT", "KUNDEPULS", "CDP_API", "DATABRICKS", "PAYMENT"
+      "RECEIPT", "SEAT", "KUNDEPULS", "CDP_API", "DATABRICKS", "PAYMENT", "BOOKING"
     ], var.privacy_service_enum)
     error_message = "Invalid privacy service enum. Check the module variables"
   }
